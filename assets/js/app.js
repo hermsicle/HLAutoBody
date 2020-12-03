bfb5b513e62ce1d2e4ab0166df0dd4d4150b538e
Herman Liu <hermanliu95@gmail.com>
2020-12-02 17:58:07 -0800

    changed color scheme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector('#hamburger');
 const navUL = document.querySelector('#nav-ul');
+const logo = document.querySelector('.logo');
 
 hamburger.addEventListener('click' , () => {
     navUL.classList.toggle('show')
+})
+
+logo.addEventListener('click' , () => {
+    window.location.href="../../index.html"
 })
