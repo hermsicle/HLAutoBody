6b37a4311d9dcb53a84471bb65256491eba06735
Herman Liu <hermanliu95@gmail.com>
2020-12-04 18:57:27 -0800

    changed styling

--- scrapers.js
+++ scrapers.js
@@ -5,15 +5,24 @@ async function scrapeProduct(url) {
     const page = await browser.newPage();
     await page.goto(url);
 
-    const [el] = await page.$x('/html/body/div[2]/div[3]/div/div[3]/div/div/div[2]/div/div/div[1]/div/div[1]/div[3]/section/div[2]/div/ul/li[1]/div/div[2]/div[3]/p/span');
-    const src = await el.getProperty('textContent');
-    const name = await src.jsonValue();
+    const docs = await page.evaluate(() => {
+        console.log(document)
+        // return {
+        //   title: document.documentElement.clientWidth,
+        //   desc: document.documentElement.clientHeight
+        // };
+      });
+    console.log(docs)
 
-    console.log({name})
-
+    // const [el] = await page.$x('/html/body/div[2]/div[3]/div/div[3]/div/div/div[2]/div/div/div[1]/div/div[1]/div[3]/section/div[2]/div/ul/li[1]/div/div[2]/div[3]/p/span');
+    // const src = await el.getProperty('textContent');
+    // const name = await src.jsonValue();
+    // console.log({name})
+    // console.log(page)
     await browser.close();
 
 }
+
 
 //Invoke Function
 scrapeProduct('https://www.yelp.com/biz/h-and-l-auto-body-oakland');
