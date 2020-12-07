953442b91be2171c246585856bb04035634581c0
Herman Liu <hermanliu95@gmail.com>
2020-12-06 17:26:13 -0800

    added flip-card

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('#hamburger');
 const navUL = document.querySelector('#nav-ul');
 const logo = document.querySelector('.logo');
+const theCard = document.querySelector('.flip-card');
+const flipToBack= document.querySelector('#flipToBack');
+const flipToFront = document.querySelector('#flipToFront');
 
 hamburger.addEventListener('click' , () => {
     navUL.classList.toggle('show')
@@ -8,4 +11,12 @@ hamburger.addEventListener('click' , () => {
 
 logo.addEventListener('click' , () => {
     window.location.href="../../client/views/index.html"
+})
+
+flipToBack.addEventListener('click', () => {
+    theCard.style.transform = "rotateY(180deg)"
+})
+
+flipToFront.addEventListener('click' , () => {
+    theCard.style.transform = "none"
 })
