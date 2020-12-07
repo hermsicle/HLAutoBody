becefa6ddb100ec305d8f8b793f93d56e94c575f
Herman Liu <hermanliu95@gmail.com>
2020-12-06 19:21:35 -0800

    added carousel

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -5,6 +5,12 @@ const theCard = document.querySelector('.flip-card');
 const flipToBack= document.querySelector('#flipToBack');
 const flipToFront = document.querySelector('#flipToFront');
 
+const slides = document.getElementsByClassName('carousel-item');
+const prevButton = document.querySelector('#carousel-button-prev');
+const nextButton = document.querySelector('#carousel-button-next');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
 hamburger.addEventListener('click' , () => {
     navUL.classList.toggle('show')
 })
@@ -20,3 +26,39 @@ flipToBack.addEventListener('click', () => {
 flipToFront.addEventListener('click' , () => {
     theCard.style.transform = "none"
 })
+
+prevButton.addEventListener('click' , moveToPrevSlide)
+nextButton.addEventListener('click' , moveToNextSlide)
+
+function hideAllSlides(){
+    for(let slide of slides){
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
+
+function moveToNextSlide(){
+    hideAllSlides();
+
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible");
+
+}
+
+function moveToPrevSlide(){
+    hideAllSlides();
+
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible");
+
+}
