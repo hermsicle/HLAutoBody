d4924059a808013a7d9dfb52549f7c404b17da67
Herman Liu <hermanliu95@gmail.com>
2020-12-07 18:07:44 -0800

    fixed bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,5 +7,5 @@ hamburger.addEventListener('click' , () => {
 })
 
 logo.addEventListener('click' , () => {
-    window.location.href="../../client/views/index.html"
+    window.location.href="../../home.html"
 })
